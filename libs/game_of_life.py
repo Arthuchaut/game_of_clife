df296f2fe19d863be64adb35c3c696e26541dd5a
arthuchaut <arthuchaut@gmail.com>
2020-06-15 22:23:08 +0200

    Added the choice of area topology and improved classes implementation

--- libs/game_of_life.py
+++ libs/game_of_life.py
@@ -1,54 +1,63 @@
 from typing import List, Tuple
+from datetime import datetime
 from random import randint
-from enum import Enum
+from enum import Enum, Flag, auto
+import sys
 from pathlib import Path
 from uuid import uuid4
 import os
 import time
 
 
-class Status(Enum):
-    ALIVE: chr = 'o'
-    DEAD: chr = '  '
+class _Topology(Enum):
+    PLANE: Flag = auto()
+    SPHERICAL: Flag = auto()
+
+
+class _State(Enum):
+    ALIVE: chr = ' x '
+    DEAD: chr = '   '
 
 
 class Cell:
     _RAND_WEIGHT: int = 5
+    STATE: _State = _State
 
-    def __init__(self, *, status: Status = None, i: int = None, j: int = None):
-        self.status: Status = status if status is not None else self.gen_random_status()
+    def __init__(self, *, state: _State = None, i: int = None, j: int = None):
+        self.state: _State = state if state else self.gen_random_state()
         self.i = i
         self.j = j
 
     def born(self) -> None:
-        self.status = Status.ALIVE
+        self.state = self.STATE.ALIVE
 
     def dies(self) -> None:
-        self.status = Status.DEAD
+        self.state = self.STATE.DEAD
 
     def is_alive(self) -> bool:
-        return self.status is Status.ALIVE
+        return self.state is self.STATE.ALIVE
 
-    def gen_random_status(self) -> Status:
+    def gen_random_state(self) -> _State:
         if not randint(0, self._RAND_WEIGHT):
-            return Status.ALIVE
+            return self.STATE.ALIVE
 
-        return Status.DEAD
+        return self.STATE.DEAD
 
     def fmt(self) -> str:
-        return f'Cell(status=Status.{self.status.name})'
+        return f'Cell(state=Cell.STATE.{self.state.name})'
 
     def __str__(self) -> str:
-        return str(self.status.value)
+        return str(self.state.value)
 
 
 class GameOfLife:
     _SLEEP_TIME: float = 0.01
+    TOPOLOGY: _Topology = _Topology
 
     def __init__(self, area: List[List[Cell]]):
         self._area: List[List[Cell]] = area
+        self._area_topology: _Topology = self.TOPOLOGY.SPHERICAL
         self._historic: Historic = None
-
         self._ref_frame_rate: float = time.time()
         self._ref_duration: float = time.time()
         self._generation: int = 0
@@ -63,11 +72,19 @@ class GameOfLife:
         self._draw_area()
 
         while True:
-            time.sleep(self._SLEEP_TIME)
-            self._update_area()
-            self._clear_area()
-            self._draw_area()
-            self._print_infos()
+            try:
+                time.sleep(self._SLEEP_TIME)
+                self._update_area()
+                self._clear_area()
+                self._draw_area()
+                self._print_infos()
+            except KeyboardInterrupt:
+                if self._historic:
+                    self._historic.trace(
+                        self._historic.area_to_str(self._area)
+                    )
+
+                sys.exit()
 
     def _draw_area(self) -> None:
         for row in self._area:
@@ -84,22 +101,40 @@ class GameOfLife:
                 if new_cell:
                     updated_cells += [new_cell]
 
-        for cell, status in updated_cells:
-            cell.status = status
+        for cell, state in updated_cells:
+            cell.state = state
             self._area[cell.i][cell.j] = cell
 
-    def _update_cell(self, cell: Cell) -> Tuple[Cell, Status]:
+    def _update_cell(self, cell: Cell) -> Tuple[Cell, _State]:
         neighbors: List[Cell] = self._neighbors(cell)
         ngb_alives: int = len([c for c in neighbors if c.is_alive()])
 
         if ngb_alives == 3:
             if not cell.is_alive():
-                return cell, Status.ALIVE
+                return cell, cell.STATE.ALIVE
         elif ngb_alives < 2 or ngb_alives > 3:
             if cell.is_alive():
-                return cell, Status.DEAD
+                return cell, cell.STATE.DEAD
 
     def _neighbors(self, cell: Cell) -> List[Cell]:
+        if self._area_topology is self.TOPOLOGY.SPHERICAL:
+            return self._spherical_neighbors(cell)
+
+        return self._plane_neighbors(cell)
+
+    def _plane_neighbors(self, cell: Cell) -> List[Cell]:
+        return [
+            self._area[i][j]
+            for j in range(cell.j - 1, cell.j + 2)
+            for i in range(cell.i - 1, cell.i + 2)
+            if (j != cell.j or i != cell.i)
+            and j > -1
+            and j < len(self._area[cell.i])
+            and i > -1
+            and i < len(self._area)
+        ]
+
+    def _spherical_neighbors(self, cell: Cell) -> List[Cell]:
         ngbs: List[Cell] = []
 
         for i in range(cell.i - 1, cell.i + 2):
@@ -124,10 +159,13 @@ class GameOfLife:
 
         return ngbs
 
+    def set_topology(self, topology: _Topology) -> None:
+        self._area_topology = topology
+
     def _clear_area(self) -> None:
         os.system('clear')
 
-    def trace_historic(self, folder: Path) -> None:
+    def set_historic(self, folder: Path) -> None:
         self._historic = Historic(folder)
 
     def _print_infos(self) -> None:
@@ -155,10 +193,16 @@ class GameOfLife:
 
     @staticmethod
     def from_str(area: str) -> List[List[Cell]]:
+        area = area.replace(' ', '')
+
         return GameOfLife.from_matrix(
             [
                 [
-                    Cell(status=Status.ALIVE if char == 'x' else Status.DEAD)
+                    Cell(
+                        state=Cell.STATE.ALIVE
+                        if char == 'x'
+                        else Cell.STATE.DEAD
+                    )
                     for char in line
                 ]
                 for line in area.split('\n')[1:-1]
@@ -169,7 +213,7 @@ class GameOfLife:
     def from_matrix(area: List[List[Cell]]) -> List[List[Cell]]:
         return [
             [
-                Cell(i=i, j=j, status=area[i][j].status)
+                Cell(i=i, j=j, state=area[i][j].state)
                 for j in range(len(area[i]))
             ]
             for i in range(len(area))
@@ -189,19 +233,17 @@ class Historic:
         self._sessid: str = uuid4()
 
     def trace(self, info: str) -> None:
-        (self._hisdir / f'{self._sessid}.his').write_text(
-            info, encoding='utf-8'
-        )
+        with (self._hisdir / f'{self._sessid}.his').open(
+            'a', encoding='utf-8'
+        ) as f:
+            f.write(f'{datetime.now()}\n{info}\n')
 
     def area_to_str(self, area: List[List[Cell]]) -> str:
-        fmt: str = '[\n'
+        fmt: str = ''
 
         for row in area:
-            fmt += '\t[\n'
             for cell in row:
-                fmt += f'\t\t{cell.fmt()},\n'
-            fmt += '\t],\n'
-
-        fmt += ']'
+                fmt += ' x ' if cell.is_alive() else ' . '
+            fmt += '\n'
 
         return fmt
